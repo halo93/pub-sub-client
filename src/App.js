b3cd31690ac5c3b0997697da84318b0343de9e35
Long Do <dohalong1993@gmail.com>
2022-10-20 13:12:16 +0200

    Feature: Add Preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./App.css";
 import Tab from "./components/Tab";
 
@@ -13,6 +13,15 @@ function App() {
 
   const [getResult, setGetResult] = useState(null);
   const [postResult, setPostResult] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  },[]);
 
   const fortmatResponse = (res) => {
     return JSON.stringify(res, null, 2);
@@ -114,54 +123,63 @@ function App() {
   };
 
   return (
-    <div id="app" className="container my-12">
-      <h3 style={{ marginTop : '50px', marginBottom: '30px', textAlign: 'center' }}>Simple Pub-sub Service</h3>
+      <div id="app" className="container">
+      {loading ? (
+            <div id="preloader">
+              <section id="status"><span>Hi there!<br/>Please wait for the connection</span></section>
+            </div>
+      ) : (
+            <div className="my-12">
+              <h3 style={{ marginTop : '50px', marginBottom: '30px', textAlign: 'center' }}>Simple Pub-sub Service</h3>
+              <div className="card mt-12">
+                <div className="card-header">Please choose the operation below to use our service</div>
+                <Tab />
+              </div>
 
-      {/*<div className="card mt-3">*/}
-      {/*  <div className="card-header">React Axios GET </div>*/}
-      {/*  <div className="card-body">*/}
-      {/*    <div className="input-group input-group-sm">*/}
-      {/*      <button className="btn btn-sm btn-primary" onClick={getAllData}>Get All</button>*/}
-
-      {/*      <input type="text" ref={get_id} className="form-control ml-2" placeholder="Id" />*/}
-      {/*      <div className="input-group-append">*/}
-      {/*        <button className="btn btn-sm btn-primary" onClick={getDataById}>Get by Id</button>*/}
-      {/*      </div>*/}
-
-      {/*      <input type="text" ref={get_title} className="form-control ml-2" placeholder="Title" />*/}
-      {/*      <div className="input-group-append">*/}
-      {/*        <button className="btn btn-sm btn-primary" onClick={getDataByTitle}>Find By Title</button>*/}
-      {/*      </div>*/}
-
-      {/*      <button className="btn btn-sm btn-warning ml-2" onClick={clearGetOutput}>Clear</button>*/}
-      {/*    </div>   */}
-      {/*    */}
-      {/*    { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      {/*<div className="card mt-3">*/}
-      {/*  <div className="card-header">React Axios POST - BezKoder.com</div>*/}
-      {/*  <div className="card-body">*/}
-      {/*    <div className="form-group">*/}
-      {/*      <input type="text" className="form-control" ref={post_title} placeholder="Title" />*/}
-      {/*    </div>*/}
-      {/*    <div className="form-group">*/}
-      {/*      <input type="text" className="form-control" ref={post_description} placeholder="Description" />*/}
-      {/*    </div>*/}
-      {/*    <button className="btn btn-sm btn-primary" onClick={postData}>Post Data</button>*/}
-      {/*    <button className="btn btn-sm btn-warning ml-2" onClick={clearPostOutput}>Clear</button>*/}
-
-      {/*    { postResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{postResult}</pre></div> }*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      <div className="card mt-12">
-        <div className="card-header">Please choose the operation below to use our service</div>
-        <Tab />
+            </div>
+            )}
       </div>
-
-    </div>
-  );
+  )
 }
 
 export default App;
+
+{/*<div className="card mt-3">*/}
+{/*  <div className="card-header">React Axios GET </div>*/}
+{/*  <div className="card-body">*/}
+{/*    <div className="input-group input-group-sm">*/}
+{/*      <button className="btn btn-sm btn-primary" onClick={getAllData}>Get All</button>*/}
+
+{/*      <input type="text" ref={get_id} className="form-control ml-2" placeholder="Id" />*/}
+{/*      <div className="input-group-append">*/}
+{/*        <button className="btn btn-sm btn-primary" onClick={getDataById}>Get by Id</button>*/}
+{/*      </div>*/}
+
+{/*      <input type="text" ref={get_title} className="form-control ml-2" placeholder="Title" />*/}
+{/*      <div className="input-group-append">*/}
+{/*        <button className="btn btn-sm btn-primary" onClick={getDataByTitle}>Find By Title</button>*/}
+{/*      </div>*/}
+
+{/*      <button className="btn btn-sm btn-warning ml-2" onClick={clearGetOutput}>Clear</button>*/}
+{/*    </div>   */}
+{/*    */}
+{/*    { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }*/}
+{/*  </div>*/}
+{/*</div>*/}
+
+{/*<div className="card mt-3">*/}
+{/*  <div className="card-header">React Axios POST - BezKoder.com</div>*/}
+{/*  <div className="card-body">*/}
+{/*    <div className="form-group">*/}
+{/*      <input type="text" className="form-control" ref={post_title} placeholder="Title" />*/}
+{/*    </div>*/}
+{/*    <div className="form-group">*/}
+{/*      <input type="text" className="form-control" ref={post_description} placeholder="Description" />*/}
+{/*    </div>*/}
+{/*    <button className="btn btn-sm btn-primary" onClick={postData}>Post Data</button>*/}
+{/*    <button className="btn btn-sm btn-warning ml-2" onClick={clearPostOutput}>Clear</button>*/}
+
+{/*    { postResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{postResult}</pre></div> }*/}
+{/*  </div>*/}
+{/*</div>*/}
+
