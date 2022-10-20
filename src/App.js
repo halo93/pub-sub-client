67a0e603c3d06e355eba9f4df619e4986b26ff53
Long Do <dohalong1993@gmail.com>
2022-10-20 12:24:56 +0200

    Feature: customize code base + add tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
+import Tab from "./components/Tab";
 
 import apiClient from "./http-common";
 
@@ -10,17 +11,8 @@ function App() {
   const post_title = useRef(null);
   const post_description = useRef(null);
 
-  const put_id = useRef(null);
-  const put_title = useRef(null);
-  const put_description = useRef(null);
-  const put_published = useRef(null);
-
-  const delete_id = useRef(null);
-
   const [getResult, setGetResult] = useState(null);
   const [postResult, setPostResult] = useState(null);
-  const [putResult, setPutResult] = useState(null);
-  const [deleteResult, setDeleteResult] = useState(null);
 
   const fortmatResponse = (res) => {
     return JSON.stringify(res, null, 2);
@@ -113,72 +105,6 @@ function App() {
     }
   }
 
-  async function putData() {
-    const id = put_id.current.value;
-
-    if (id) {
-      const putData = {
-        title: put_title.current.value,
-        description: put_description.current.value,
-        published: put_published.current.checked,
-      };
-
-      try {
-        const res = await apiClient.put(`/tutorials/${id}`, putData, {
-          headers: {
-            "x-access-token": "token-value",
-          },
-        });
-
-        const result = {
-          status: res.status + "-" + res.statusText,
-          headers: res.headers,
-          data: res.data,
-        };
-
-        setPutResult(fortmatResponse(result));
-      } catch (err) {
-        setPutResult(fortmatResponse(err.response?.data || err));
-      }
-    }
-  }
-
-  async function deleteAllData() {
-    try {
-      const res = await apiClient.delete("/tutorials");
-
-      const result = {
-        status: res.status + "-" + res.statusText,
-        headers: res.headers,
-        data: res.data,
-      };
-
-      setDeleteResult(fortmatResponse(result));
-    } catch (err) {
-      setDeleteResult(fortmatResponse(err.response?.data || err));
-    }
-  }
-
-  async function deleteDataById() {
-    const id = delete_id.current.value;
-
-    if (id) {
-      try {
-        const res = await apiClient.delete(`/tutorials/${id}`);
-
-        const result = {
-          status: res.status + "-" + res.statusText,
-          headers: res.headers,
-          data: res.data,
-        };
-
-        setDeleteResult(fortmatResponse(result));
-      } catch (err) {
-        setDeleteResult(fortmatResponse(err.response?.data || err));
-      }
-    }
-  }
-
   const clearGetOutput = () => {
     setGetResult(null);
   };
@@ -187,98 +113,53 @@ function App() {
     setPostResult(null);
   };
 
-  const clearPutOutput = () => {
-    setPutResult(null);
-  };
-
-  const clearDeleteOutput = () => {
-    setDeleteResult(null);
-  };
-
   return (
-    <div id="app" className="container my-3">
-      <h3>React Axios example</h3>
+    <div id="app" className="container my-12">
+      <h3 style={{ marginTop : '50px', marginBottom: '30px', textAlign: 'center' }}>Simple Pub-sub Service</h3>
 
-      <div className="card mt-3">
-        <div className="card-header">React Axios GET - BezKoder.com</div>
-        <div className="card-body">
-          <div className="input-group input-group-sm">
-            <button className="btn btn-sm btn-primary" onClick={getAllData}>Get All</button>
+      {/*<div className="card mt-3">*/}
+      {/*  <div className="card-header">React Axios GET </div>*/}
+      {/*  <div className="card-body">*/}
+      {/*    <div className="input-group input-group-sm">*/}
+      {/*      <button className="btn btn-sm btn-primary" onClick={getAllData}>Get All</button>*/}
 
-            <input type="text" ref={get_id} className="form-control ml-2" placeholder="Id" />
-            <div className="input-group-append">
-              <button className="btn btn-sm btn-primary" onClick={getDataById}>Get by Id</button>
-            </div>
+      {/*      <input type="text" ref={get_id} className="form-control ml-2" placeholder="Id" />*/}
+      {/*      <div className="input-group-append">*/}
+      {/*        <button className="btn btn-sm btn-primary" onClick={getDataById}>Get by Id</button>*/}
+      {/*      </div>*/}
 
-            <input type="text" ref={get_title} className="form-control ml-2" placeholder="Title" />
-            <div className="input-group-append">
-              <button className="btn btn-sm btn-primary" onClick={getDataByTitle}>Find By Title</button>
-            </div>
+      {/*      <input type="text" ref={get_title} className="form-control ml-2" placeholder="Title" />*/}
+      {/*      <div className="input-group-append">*/}
+      {/*        <button className="btn btn-sm btn-primary" onClick={getDataByTitle}>Find By Title</button>*/}
+      {/*      </div>*/}
 
-            <button className="btn btn-sm btn-warning ml-2" onClick={clearGetOutput}>Clear</button>
-          </div>   
-          
-          { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }
-        </div>
+      {/*      <button className="btn btn-sm btn-warning ml-2" onClick={clearGetOutput}>Clear</button>*/}
+      {/*    </div>   */}
+      {/*    */}
+      {/*    { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+
+      {/*<div className="card mt-3">*/}
+      {/*  <div className="card-header">React Axios POST - BezKoder.com</div>*/}
+      {/*  <div className="card-body">*/}
+      {/*    <div className="form-group">*/}
+      {/*      <input type="text" className="form-control" ref={post_title} placeholder="Title" />*/}
+      {/*    </div>*/}
+      {/*    <div className="form-group">*/}
+      {/*      <input type="text" className="form-control" ref={post_description} placeholder="Description" />*/}
+      {/*    </div>*/}
+      {/*    <button className="btn btn-sm btn-primary" onClick={postData}>Post Data</button>*/}
+      {/*    <button className="btn btn-sm btn-warning ml-2" onClick={clearPostOutput}>Clear</button>*/}
+
+      {/*    { postResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{postResult}</pre></div> }*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      <div className="card mt-12">
+        <div className="card-header">Please choose the operation below to use our service</div>
+        <Tab />
       </div>
 
-      <div className="card mt-3">
-        <div className="card-header">React Axios POST - BezKoder.com</div>
-        <div className="card-body">
-          <div className="form-group">
-            <input type="text" className="form-control" ref={post_title} placeholder="Title" />
-          </div>
-          <div className="form-group">
-            <input type="text" className="form-control" ref={post_description} placeholder="Description" />
-          </div>
-          <button className="btn btn-sm btn-primary" onClick={postData}>Post Data</button>
-          <button className="btn btn-sm btn-warning ml-2" onClick={clearPostOutput}>Clear</button>
-
-          { postResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{postResult}</pre></div> }
-        </div>
-      </div>
-
-      <div className="card mt-3">
-        <div className="card-header">React Axios PUT - BezKoder.com</div>
-        <div className="card-body">
-          <div className="form-group">
-            <input type="text" className="form-control" ref={put_id} placeholder="Id" />
-          </div>
-          <div className="form-group">
-            <input type="text" className="form-control" ref={put_title} placeholder="Title" />
-          </div>
-          <div className="form-group">
-            <input type="text" className="form-control" ref={put_description} placeholder="Description" />
-          </div>
-          <div className="form-check mb-2">
-            <input type="checkbox" className="form-check-input" ref={put_published} />
-            <label className="form-check-label" htmlFor="put_published">Publish</label>
-          </div>
-          <button className="btn btn-sm btn-primary" onClick={putData}>Update Data</button>
-          <button className="btn btn-sm btn-warning ml-2" onClick={clearPutOutput}>Clear</button>
-
-          { putResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{putResult}</pre></div> }
-        </div>
-      </div>
-
-      <div className="card mt-3">
-        <div className="card-header">React Axios DELETE - BezKoder.com</div>
-        <div className="card-body">
-          <div className="input-group input-group-sm">
-            <button className="btn btn-sm btn-danger" onClick={deleteAllData}>Delete All</button>
-
-            <input type="text" ref={delete_id} className="form-control ml-2" placeholder="Id" />
-            <div className="input-group-append">
-              <button className="btn btn-sm btn-danger" onClick={deleteDataById}>Delete by Id</button>
-            </div>
-
-            <button className="btn btn-sm btn-warning ml-2" onClick={clearDeleteOutput}>Clear</button>
-          </div>    
-          
-          { deleteResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{deleteResult}</pre></div> }      
-        </div>
-      </div>
- 
     </div>
   );
 }
---
+++ src/components/FirstTab.jsx
@@ -0,0 +1,15 @@
+// import {useEffect} from "react";
+
+const FirstTab = ({ clean }) => {
+
+    // useEffect(() => {
+    //     if (clean) {
+    //
+    //     }
+    // }, []);
+
+    return (
+        <h1>Send Message tab</h1>
+    );
+};
+export default FirstTab;
